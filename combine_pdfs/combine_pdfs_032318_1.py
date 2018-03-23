a375fe5a6a4c9f3eeb50624ed48011e068378326
Sunny Lam <sunny.lam@gmail.com>
2018-03-23 15:56:04 -0400

    revised code

--- combine_pdfs/combine_pdfs_032318_1.py
+++ combine_pdfs/combine_pdfs_032318_1.py
@@ -11,3 +11,21 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
 # logging.disable(logging.CRITICAL)
 
+# get all the pdf filenames
+
+pdfFiles = []
+
+for filename in os.listdir('.'):
+	if filename.endswith('.pdf'):
+		pdfFiles.append(filename)
+
+pdfFiles.sort(key/str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# TODO:  loop through all the PDF files
+
+# TODO:  loop through all the pages (except the first) and add them
+
+# TODO:  save the resulting PDF to a file
+
